fa936ccfee519b8d7666504ac8db217b9d411b27
rustamzh <ruszh96@gmail.com>
2019-04-01 19:55:13 +0600

    fix .travis

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash 
 
-set -x
 
-echo "Changing directory"
+
 cd dist
-echo "Changed directory"
 TOKEN=$(curl -X POST "https://auth.rustamzh.com/auth/realms/Vqms/protocol/openid-connect/token" -d "client_id=vqms&username="$USERNAME"&password="$PASSWORD"&grant_type=password" | jq -r .access_token)
-echo "Got token"
 curl -X DELETE "$URL/files?version=1.0&businessId=$BUSINESSID" -H "Authorization: Bearer $TOKEN"
-echo "Deleted old files"
-find * -type f | xargs -I {} -n 1 -t curl -X POST "$URL/files?version=1.0&relativePath={}&businessId=$BUSINESSID" -H "Authorization: Bearer $TOKEN" -F "file=@$PWD/{}"
-echo "Uploaded new files"
+find * -type f | xargs -I {} -n 1 curl -X POST "$URL/files?version=1.0&relativePath=/{}&businessId=$BUSINESSID" -H "Authorization: Bearer $TOKEN" -F "file=@$PWD/{}"
